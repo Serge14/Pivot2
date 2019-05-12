62761cb760dcff420e7c4496fe9e9d178f2f0c65
Sergiy <sobukhov@gmail.com>
2019-05-12 20:35:48 +0300

    need furrther work

--- addSubBrand.R
+++ addSubBrand.R
@@ -1,11 +1,15 @@
 addSubBrand = function(df){
   
   df[, SubBrand := str_extract(SKU, "[^0-9]{2,}")]
+  df[, SubBrand := str_trim(SubBrand)]
   df[, SubBrand := str_remove(SubBrand, " P$")]
   df[, SubBrand := str_remove(SubBrand, " L$")]
   df[, SubBrand := str_remove(SubBrand, " C$")]
   df[, SubBrand := str_remove(SubBrand, " CW$")]
   df[, SubBrand := str_remove(SubBrand, " CM$")]
+  df[, SubBrand := str_remove(SubBrand, "/$")]
+  df[, SubBrand := str_trim(SubBrand)]
+  df[, SubBrand := str_to_title(SubBrand)]
   
 }
 
