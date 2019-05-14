57caf276fdc154e46c071ffc8afaca9a959a453c
Sergiy <sobukhov@gmail.com>
2019-05-15 00:06:11 +0300

    2015

--- processing.R
+++ processing.R
@@ -7,8 +7,8 @@ source("/home/sergiy/Documents/Work/Nutricia/Scripts/Pivot2/addAge.R")
 source("/home/sergiy/Documents/Work/Nutricia/Scripts/Pivot2/addPS2.R")
 source("/home/sergiy/Documents/Work/Nutricia/Scripts/Pivot2/addSubBrand.R")
 setwd("/home/sergiy/Documents/Work/Nutricia/Rework/201903")
-df = fread("N_Y2018-Y19_M03.csv", check.names = TRUE)
-df = fread("N_Y2018-Y19_M03_value.csv", check.names = TRUE, na.strings = "NA")
+df = fread("N_Y2015-Y19_M03.csv", check.names = TRUE, na.strings = "NA")
+df1 = fread("N_Y2015-Y19_M03_value.csv", check.names = TRUE, na.strings = "NA")
 
 setwd("/home/sergiy/Documents/Work/Nutricia/Scripts/Pivot2")
 dictCompanyBrand = fread("dictCompanyBrand.csv")
@@ -16,13 +16,19 @@ dictSegments = fread("dictSegments.csv")
 dictEC = fread("dictEC.csv")
 dictPriceSegments = fread("/home/sergiy/Documents/Work/Nutricia/Rework/Dictionaries/PriceSegments.csv")
 
-df[grepl("PYURE", SKU), PRODUCT.FORM := "Pure"]
 
 cols = c("SKU", "BRAND", "BRAND.OWNER",
           "DANONE.SEGMENT", "DANONE.SUB.SEGMENT",
           "PRODUCT.FORM", "TYPE...BABY.PRODUCT")
 
 df = melt.data.table(df, id.vars = cols)
+df1 = melt.data.table(df1, id.vars = cols)
+
+names(df)[9] = "Volume"
+
+df[df1, on = c("SKU", "variable"), Value := i.value]
+
+df[grepl("PYURE", SKU), PRODUCT.FORM := "Pure"]
 
 # df$value = as.numeric(df$value)
 
@@ -62,10 +68,14 @@ df[, Mnb := str_extract(variable, "[A-Za-z]+")]
 df[dictMonth, on = .(Mnb = Month), Mnb := i.Mnb]
 
 # Sales
-df[is.na(value), value := 0]
-df[, Volume := value*1000]
+# df[is.na(value), value := 0]
+df = df[Value != "NA"]
+df[is.na(Volume), Volume := 0]
+df[, `:=`(Volume = Volume*1000,
+          Value = Value*1000)]
 
-df = df[, .(Volume = sum(Volume)), 
+df = df[, .(Volume = sum(Volume),
+            Value = sum(Value)), 
         by = .(SKU, Ynb, Mnb, 
                Brand, SubBrand, Size, Age, Company, 
                PS0, PS3, PS2, PS, PRODUCT.FORM)]
@@ -92,14 +102,16 @@ df1 = fread("/home/sergiy/Documents/Work/Nutricia/1/Data/df.csv")
 #                  PS0, PS2, PS3, PS, Form, 
 #                  Channel)]
 
-df1 = df1[Ynb >= 2018 & Channel == "PHARMA",
-          .(SKU, Ynb, Mnb, 
-            Brand, SubBrand, Size, Age, Company, 
-            PS0, PS2, PS3, PS, Form, 
-            Channel,
-            Volume)]
+# df1 = df1[Ynb >= 2018 & Channel == "PHARMA",
+#           .(SKU, Ynb, Mnb, 
+#             Brand, SubBrand, Size, Age, Company, 
+#             PS0, PS2, PS3, PS, Form, 
+#             Channel,
+#             Volume, Value)]
 
-df1 = df1[, .(Volume = sum(Volume)),
+df1 = df1[Ynb >= 2015 & Channel == "PHARMA",
+          .(Volume = sum(Volume),
+            Value = sum(Value)),
           by = .(SKU, Ynb, Mnb, 
                  Brand, SubBrand, Size, Age, Company, 
                  PS0, PS2, PS3, PS, Form, 
@@ -109,11 +121,11 @@ df = df[, .(SKU, Ynb, Mnb,
             Brand, SubBrand, Size, Age, Company, 
             PS0, PS2, PS3, PS, Form, 
             Channel,
-            Volume)]
+            Volume, Value)]
 
 all(names(df) == names(df1))
 
-df = rbindlist(list(df[Volume > 0], df1))
+df = rbindlist(list(df, df1))
 
 # df = rbindlist(list(df[Value > 0], df1))
 
@@ -166,7 +178,8 @@ df[PS == "Hypoallergenic" & Channel == "MT", EC := 1.1]
 df[PS == "Hypoallergenic" & Channel == "PHARMA", EC := 1.1]
 df[is.na(EC), .N]
 
-df[, VolumeC := Volume*EC]
+df[, `:=`(VolumeC = Volume*EC,
+          ValueC = Value*EC)]
 # df[, ValueC := Value*EC]
 
 df = df[, .(SKU, Ynb, Mnb,
@@ -174,7 +187,7 @@ df = df[, .(SKU, Ynb, Mnb,
             PS0, PS2, PS3, PS,
             Form, Channel, 
             PriceSegment, GlobalPriceSegment,
-            Volume, EC, VolumeC)]
+            Volume, Value, EC, VolumeC, ValueC)]
 
 # df = df[, .(SKU, Ynb, Mnb,
 #             Brand, SubBrand, Size, Age, Company,
@@ -184,4 +197,4 @@ df = df[, .(SKU, Ynb, Mnb,
 #             Value, EC, ValueC)]
 
 setwd("/home/sergiy/Documents/Work/Nutricia/Rework/201903")
-fwrite(df, "df_MT_PH_SKU.csv", row.names = FALSE)
+fwrite(df, "df_MT_PH_SKU_from15.csv", row.names = FALSE)
